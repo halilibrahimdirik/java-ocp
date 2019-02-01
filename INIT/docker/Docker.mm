b7cce2546ec1bd012aa45e2758cf36c145477903
hidirik <hidirik@ds-hidirik>
2019-02-01 10:55:26 +0300

    Dokcer.mm

---
+++ INIT/docker/Docker.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1549007646856" ID="ID_1242769401" MODIFIED="1549007663115" TEXT="Docker"/>
+</map>
